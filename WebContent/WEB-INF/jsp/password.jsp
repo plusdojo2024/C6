95ce5d026cc5ea5865527733069ab15dafa7c4a7
plusdojo2024-ogawa <ogawa-mami-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 14:35:56 +0900

    s

--- WebContent/WEB-INF/jsp/password.jsp
+++ WebContent/WEB-INF/jsp/password.jsp
@@ -4,9 +4,23 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>パスワード変更</title>
+<link rel="stylesheet" href="/C6/css/common.css">
 </head>
 <body>
-
+	<h1>new password</h1>
+	<div>password</div>
+	<div>
+		<input type="text" name="passwordUp" >
+	</div>
+	<div>
+		<input type="text" name="passwordDown" >
+	</div>
+	<div>
+		<input type="button" name="change" value="registration">
+	</div>
+	<div>
+		<a href="/C6/LoginServlet"  >ログイン画面に戻る</a>
+	</div>
 </body>
 </html>
--- src/servlet/PasswordServlet.java
+++ src/servlet/PasswordServlet.java
@@ -1,5 +1,19 @@
 package servlet;
 
-public class PasswordServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/PasswordServlet")
+public class PasswordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	}
 
 }
