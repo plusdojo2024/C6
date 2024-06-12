4a25df9ad820b07ebfde913ce53a58b571f5d45a
plusdojo2024-katayama <katayama-tsubasa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 16:11:05 +0900

    Update LogoutServlet.java

--- src/servlet/LogoutServlet.java
+++ src/servlet/LogoutServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
@@ -15,7 +16,14 @@ public class LogoutServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// ログインページにリダイレクトする
 		res.sendRedirect("/c6/LoginServlet");
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = req.getSession();
+		if (session.getAttribute("id") == null) {
+			res.sendRedirect("/C6/LoginServlet");
+			return;
+		}
 	}
+
 
 
 }
