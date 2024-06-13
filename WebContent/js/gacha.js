6a95129e9746603d2492a21e2299181e8225d637
plusdojo2024-sugawara <sugawara-isshin-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 16:44:20 +0900

    ppp

--- WebContent/js/gacha.js
+++ WebContent/js/gacha.js
@@ -1,3 +1,23 @@
-/**
- * 
- */
+   'use strict'
+
+// <js>ガチャを引くボタンを検知
+// <js>ガチャエラー
+//     1日に１回
+//		のみべを登録してください
+// <js>数字をランダム生成
+// <ser>数字とユーザーをSQLのコレクションに送る
+// <ser>ランダム数字と同じ酒名をSQLのitems_idからもってくる
+// <js>windows.alertで結果を表示
+
+
+
+
+    //const omikujiResults = SQLのコレクションにする;
+    const button = document.getElementById('gacha');
+
+    button.addEventListener('click', function() {
+        const randomIndex = Math.floor(Math.random() * omikujiResults.length);
+        const result = omikujiResults[randomIndex];
+
+        window.alert('あなたの運勢は『' + result + '』です！');
+    });
