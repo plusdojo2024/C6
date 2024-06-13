82ce87ebb012adacbb9ad928a8d4baa7cb413d1e
plusdojo2024-katayama <katayama-tsubasa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 11:04:48 +0900

    a

--- src/servlet/NumberServlet.java
+++ src/servlet/NumberServlet.java
@@ -8,23 +8,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/NumberServlet")
 public class NumberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = req.getSession();
-		if (session.getAttribute("id") == null) {
-			res.sendRedirect("/c6/LoginServlet");
-			return;
-		}
-		// ユーザーページにフォワードする
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/number.jsp");
 		dispatcher.forward(req, res);
+
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//doGet(req, res);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
+		dispatcher.forward(req, res);
+
 	}
 
 }
-
