b9d0db0c86fa1d434af66a7864e31306d31cf74b
plusdojo2024-terashima <terashima-yuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 14:37:24 +0900

    a

--- src/dao/CollectionsDao.java
+++ src/dao/CollectionsDao.java
@@ -1,5 +1,150 @@
 package dao;
 
-public class CollectionsDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Collections;
+
+public class CollectionsDAO {
+
+
+	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
+	public boolean insert(Collections card) throws Exception {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			//Connctionする
+			 conn = BaseDAO.conn();
+			// SQL文を準備する（AUTO_INCREMENTのNUMBER列にはNULLを指定する）
+			String sql = "INSERT INTO Bc VALUES (NULL, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (card.getUsers_id() != null && !card.getUsers_id().equals("")) {
+				pStmt.setInt(1, card.getUsers_id());
+			} else {
+				pStmt.setInt(1, "（未設定）");
+			}
+			if (card.getItem_id() != null && !card.getItem_id().equals("")) {
+				pStmt.setInt(2, card.getItem_id());
+			} else {
+				pStmt.setInt(2, "（未設定）");
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+	// 引数cardで指定されたレコードを更新し、成功したらtrueを返す
+	public boolean update(Collections card) throws Exception {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			//Connctionする
+			conn = BaseDAO.conn();
+
+			// SQL文を準備する
+			String sql = "UPDATE Bc SET company=?, address=?, name=?, nameHiragana=?, remarks=? WHERE number=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (card.getUsers_id() != null && !card.getUsers_id().equals("")) {
+				pStmt.setInt(1, card.getUsers_id());
+			} else {
+				pStmt.setString(1, null);
+			}
+			if (card.getItems_id() != null && !card.getItems_id().equals("")) {
+				pStmt.setInt(2, card.getAddress());
+			} else {
+				pStmt.setInt(2, null);
+
+
+			pStmt.setInt(6, card.getId());
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+			} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+	// 引数numberで指定されたレコードを削除し、成功したらtrueを返す
+	public boolean delete(int number) throws Exception {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			//Connctionする
+			conn = BaseDAO.conn();
+
+			// SQL文を準備する
+			String sql = "DELETE FROM Bc WHERE number=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, number);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
 }
