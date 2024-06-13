e9b927b70f47a5eb30a78f6730dccd3ba79ffbbc
plusdojo2024-sugawara <sugawara-isshin-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 09:53:04 +0900

    Update LoginServlet.java

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		dispatcher.forward(req, res);
