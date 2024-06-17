f919decda7ff7528d156051020521540bdc8bdad
plusdojo2024-ogawa <ogawa-mami-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 15:24:04 +0900

    フレンド追加servletと泡のcss

--- src/servlet/FriendAddServlet.java
+++ src/servlet/FriendAddServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UsersDAO;
+import model.Users;
+
 @WebServlet("/FriendAddServlet")
 public class FriendAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -20,9 +23,30 @@ public class FriendAddServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
-		dispatcher.forward(req, res);
+		try {
+			// リクエストパラメータを取得する
+			req.setCharacterEncoding("UTF-8");
+			String name = req.getParameter("name");
+			String num = req.getParameter("number");
 
+			//numberをStringからintにする
+			int number = Integer.parseInt(num);
+
+			// 友達追加処理を行う
+			UsersDAO uDAO = new UsersDAO();
+
+			//beanにセット
+			Users u = new Users();
+			u.setName(name);
+			u.setNumber(number);
+
+			//TODO友達追加処理メソッドまだない
+
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
+			dispatcher.forward(req, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
