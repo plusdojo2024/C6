79e0f297e7151fd4b073aabe2fe0039ba2ab3d7b
plusdojo2024-katayama <katayama-tsubasa-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 15:03:36 +0900

    DAOtest

--- src/test/CollectionsDaotest.java
+++ src/test/CollectionsDaotest.java
@@ -1,17 +1,16 @@
 package test;
 import java.util.List;
 
-import dao.CollectionsDao;
+import dao.CollectionsDAO;
 import model.Collections;
 
 public class CollectionsDaotest {
 	public static void main(String[] args) {
-		CollectionsDao dao = new CollectionsDao();
-		List<Collections> cardList;
+		CollectionsDAO dao = new CollectionsDAO();
+		List<Collections> cardList = null;
 
 		// selectメソッドのテスト
 		System.out.println("<< selectメソッドのテスト（すべてのレコードを検索します）>>");
-		cardList = dao.select(new Collections());
 		for (Collections card : cardList) {
 			System.out.println("コレクションID：" + card.getId());
 			System.out.println("ユーザーID：" + card.getUsers_id());
@@ -23,15 +22,20 @@ public class CollectionsDaotest {
 		int upDelNumber = 0;		// 後で更新および削除する番号
 		System.out.println("<< insertメソッドのテスト（1件のレコードを登録します）>>");
 		Collections insRec = new Collections(0, 0, 0);
-		if (dao.insert(insRec)) {
-			System.out.println("登録成功！");
-			cardList = dao.select(insRec);
-			for (Collections card : cardList) {
-				upDelNumber = card.getUsers_id();	// 最後のレコードを後で更新および削除する
+		try {
+			if (dao.insert(insRec)) {
+				System.out.println("登録成功！");
+				cardList = dao.select(insRec);
+				for (Collections card : cardList) {
+					upDelNumber = card.getUsers_id();	// 最後のレコードを後で更新および削除する
+				}
 			}
-		}
-		else {
-			System.out.println("登録失敗！");
+			else {
+				System.out.println("登録失敗！");
+			}
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
 		System.out.println();
 
