e42e042d75fd3950251a97fe092d57d09d40eaec
plusdojo2024-ogawa <ogawa-mami-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 10:11:35 +0900

    a

--- WebContent/js/newRegist.js
+++ WebContent/js/newRegist.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+/* 誕生日4桁チェック */
+function checkBirth() {
+let birthday = document.getElementById('birthday');
+	if (birthday.length === 4) {
+		return true;
+	}
+	 alert("4桁の数字で入力してください");
+	return false;
+
+}
