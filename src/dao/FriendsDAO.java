d8b30ff4a17f92ce457bb7491f13914013785516
plusdojo2024-terashima <terashima-yuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 16:45:33 +0900

    FriendsDAO

--- src/dao/FriendsDAO.java
+++ src/dao/FriendsDAO.java
@@ -23,7 +23,7 @@ public class FriendsDAO {
 		try {
 			conn=BaseDAO.conn();
 
-		String sql="select *from friends inner join users on users.id=friends.users_id where users_id = ?";
+		String sql="select *from friends inner join users on users.id=friends.users_id where friends_id = ?";
 		PreparedStatement pStmt = conn.prepareStatement(sql);
 
 		pStmt.setInt(1, id);
