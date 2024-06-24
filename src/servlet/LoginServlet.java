0d5cdbb975ebff67ec042e598099eb2630467a24
plusdojo2024-kimurayuka <kimura-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-24 16:40:47 +0900

    パスワード

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -25,37 +25,38 @@ public class LoginServlet extends HttpServlet {
 
 	}
 
-	 protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-	        try {
-	            // リクエストパラメータを取得する
-	            req.setCharacterEncoding("UTF-8");
-	            String name = req.getParameter("name");
-	            String password = req.getParameter("password");
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		try {
+			// リクエストパラメータを取得する
+			req.setCharacterEncoding("UTF-8");
+			String name = req.getParameter("name");
+			String password = req.getParameter("password");
 
-	            // ログイン処理を行う
-	            UsersDAO uDAO = new UsersDAO();
-	            Users u = new Users();
-	            u.setName(name);
-	            u.setPassword(password);
+			// ログイン処理を行う
+			UsersDAO uDAO = new UsersDAO();
+			Users u = new Users();
+			u.setName(name);
+			u.setPassword(password);
 
-	            if (uDAO.isLoginOK(u)) {
-	                // セッションオブジェクトを生成し、データを格納
-	                HttpSession session = req.getSession();
-	                session.setAttribute("name", name);
+			if (uDAO.isLoginOK(u)) {
+				// セッションオブジェクトを生成し、データを格納
+				HttpSession session = req.getSession();
+				session.setAttribute("name", name);
 
-	                // ログインに成功したら、マイページにリダイレクトする
-	                res.sendRedirect(req.getContextPath() + "/user");
-	            } else {
-	                // 失敗したらログインページに戻す
-	                req.setAttribute("message", "IDまたはパスワードが違います");
-	                RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
-	                dispatcher.forward(req, res);
-	            }
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            req.setAttribute("message", "内部エラーが発生しました。再度お試しください。");
-	            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
-	            dispatcher.forward(req, res);
-	        }
-	    }
+				// ログインに成功したら、マイページにフォワードする
+				RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
+				dispatcher.forward(req, res);
+			} else {
+				// 失敗したらログインページに戻す
+				req.setAttribute("message", "IDまたはパスワードが違います");
+				RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+				dispatcher.forward(req, res);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			req.setAttribute("message", "内部エラーが発生しました。再度お試しください。");
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(req, res);
+		}
 	}
+}
