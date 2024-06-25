8aa517062bf734e46514e3c322505e96fd71b5ff
plusdojo2024-sugawara <sugawara-isshin-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-25 15:07:58 +0900

    コレクションページ

--- WebContent/WEB-INF/jsp/collection.jsp
+++ WebContent/WEB-INF/jsp/collection.jsp
@@ -28,26 +28,20 @@
 <!-- ここからコレクション一覧を表示する -->
 
 	<c:forEach var="Items" items="${cardList}">
-		<input type= "image" class="label" name="open" src="/c6/img/${Items.label}.png">
-			<!-- ここからラベルがクリックされたら詳細を表示する -->
-			<div class = "close">
+		<input type= "image" class="label" name="${Items.label}" src="/c6/img/${Items.label}.png">
+			<div class = "close"><!-- cssで非表示。jsで使用 -->
 				<div class="${Items.label}">
-					<div class = "labelHidden"><img src="/c6/img/${Items.label}.png"></div><br>
-					<div class = "name">名前</div><br>
 					<div class = "itemsName">${Items.name}</div><br>
-					<div class = "memo">詳細</div><br>
 					<div class = "itemsMemo">${Items.memo}</div><br>
-					<input type= "submit" class="closebt" name="close" value="閉じるボタン">
-				</div><!-- ${Items.label} -->
-			</div><!-- close -->
-			<!-- ここまでラベルがクリックされたら詳細を表示する -->
+				</div>
+			</div>
 	</c:forEach>
 
 <!-- ここまでコレクション一覧を表示する -->
 
 </main>
 <%@ include file="/WEB-INF/jsp/common.jsp" %>
-<script src= "./js/collection.js"></script>
+<script src= "./js/collections.js"></script>
 <script src= "./js/common.js"></script>
 <script src="./js/jquery3.7.1.min.js"></script>
 
--- WebContent/js/collections.js
+++ WebContent/js/collections.js
@@ -1,1 +1,142 @@
 'use strict'
+
+$(document).ready(function() {
+	$('input[name="gachaResult1"]').on('click', function() {
+		var name = document.querySelector('.gachaResult1 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult1 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult2"]').on('click', function() {
+		var name = document.querySelector('.gachaResult2 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult2 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult3"]').on('click', function() {
+		var name = document.querySelector('.gachaResult3 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult3 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult4"]').on('click', function() {
+		var name = document.querySelector('.gachaResult4 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult4 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult5"]').on('click', function() {
+		var name = document.querySelector('.gachaResult5 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult5 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult6"]').on('click', function() {
+		var name = document.querySelector('.gachaResult6 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult6 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult7"]').on('click', function() {
+		var name = document.querySelector('.gachaResult7 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult7 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult8"]').on('click', function() {
+		var name = document.querySelector('.gachaResult8 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult8 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult9"]').on('click', function() {
+		var name = document.querySelector('.gachaResult9 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult9 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult10"]').on('click', function() {
+		var name = document.querySelector('.gachaResult10 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult10 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult11"]').on('click', function() {
+		var name = document.querySelector('.gachaResult11 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult11 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult12"]').on('click', function() {
+		var name = document.querySelector('.gachaResult12 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult12 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult13"]').on('click', function() {
+		var name = document.querySelector('.gachaResult13 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult13 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult14"]').on('click', function() {
+		var name = document.querySelector('.gachaResult14 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult14 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult15"]').on('click', function() {
+		var name = document.querySelector('.gachaResult15 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult15 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult16"]').on('click', function() {
+		var name = document.querySelector('.gachaResult16 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult16 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult17"]').on('click', function() {
+		var name = document.querySelector('.gachaResult17 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult17 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult18"]').on('click', function() {
+		var name = document.querySelector('.gachaResult18 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult18 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult19"]').on('click', function() {
+		var name = document.querySelector('.gachaResult19 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult19 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
+$(document).ready(function() {
+	$('input[name="gachaResult20"]').on('click', function() {
+		var name = document.querySelector('.gachaResult20 .itemsName').textContent;
+		var memo = document.querySelector('.gachaResult20 .itemsMemo').textContent;
+		window.alert('名前 \n' + name + '\n詳細 \n'+ memo);
+	});
+});
